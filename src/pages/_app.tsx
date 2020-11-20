1eb5bb39512e8e93116d1685013e7e53acdbe9b0
Andre Morpa <andremorpa@gmail.com>
2020-11-20 15:40:07 +0000

    Change images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,8 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Beers Explorer</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <meta name="theme-color" content="#06092B" />
+        <link rel="shortcut icon" href="/img/icon-192.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="The best beers in the world!" />
