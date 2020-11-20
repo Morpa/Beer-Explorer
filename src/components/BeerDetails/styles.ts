4c536797de5cdc5b8dfa6290cbb66b6272f36a96
Andre Morpa <andremorpa@gmail.com>
2020-11-20 14:39:59 +0000

    Add BeerDeatils component

--- src/components/BeerDetails/styles.ts
+++ src/components/BeerDetails/styles.ts
@@ -5,7 +5,7 @@ import media from 'styled-media-query'
 export const Layout = styled(motion.div)`
   ${({ theme }) => css`
     padding: ${theme.spacings.xsmall};
-    background: ${theme.colors.white};
+    background: ${theme.colors.lightBg};
   `}
 `
 
@@ -18,7 +18,7 @@ export const BeerImage = styled.img`
     height: 12.5rem;
     object-fit: contain;
     border-radius: 50%;
-    border: 2px solid ${theme.colors.white};
+    border: 2px solid ${theme.colors.secondary};
     padding: ${theme.spacings.xxsmall};
   `}
 `
@@ -30,17 +30,28 @@ export const BeerTitle = styled.h2`
   `}
 `
 
+export const BeerDescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem 0;
+`
+
+export const BeerText = styled.p``
+
 export const SectionTitle = styled.h3`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.xlarge};
     font-weight: bold;
     padding-top: ${theme.spacings.small};
-    border-top: 1px solid ${theme.colors.white};
+    padding-bottom: ${theme.spacings.xsmall};
+    border-top: 1px solid ${theme.colors.secondary};
   `}
 `
 
 export const SubsectionTitle = styled.h4`
   font-style: italic;
+  padding-top: 2rem;
 `
 
 export const DetailsList = styled.dl`
@@ -50,10 +61,14 @@ export const DetailsList = styled.dl`
     grid-gap: 1rem;
 
     dt {
+      display: flex;
+      align-items: center;
+      justify-content: space-baseline;
       text-align: right;
     }
 
     dt span {
+      margin-left: 1rem;
       font-size: ${theme.font.sizes.xsmall};
     }
 
@@ -65,4 +80,49 @@ export const DetailsList = styled.dl`
       grid-template-columns: auto 1fr auto 1fr;
     `}
   `}
+`
+
+export const IngredientList = styled.dl`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 1rem;
+
+    dt {
+      display: inline-flex;
+      align-items: center;
+      font-weight: ${theme.font.bold};
+    }
+
+    dt span {
+      margin-left: 1ch;
+      font-weight: normal;
+      font-size: ${theme.font.sizes.xsmall};
+    }
+  `}
+`
+export const Highlight = styled.div`
+  border-radius: 0.4rem;
+  padding: 2rem;
+
+  background: linear-gradient(to left, #fdc830, #f37335);
+  text-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+`
+
+export const DiscList = styled.ul`
+  ${({ theme }) => css`
+    padding-left: ${theme.spacings.xsmall};
+
+    li {
+      list-style-type: disc;
+    }
+  `}
+`
+
+export const ServedIngredients = styled.p`
+  margin: 1rem 0;
+`
+
+export const Contributed = styled.p`
+  margin: 1rem 0;
 `
--- src/pages/beer/[beerId].tsx
+++ src/pages/beer/[beerId].tsx
@@ -1,11 +1,161 @@
-import Base from 'templates/Base'
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
 
-const Beer = () => {
+import api from 'services/api'
+
+import Base from 'templates/Base'
+import { Beer } from 'types/Beer'
+
+import * as S from 'components/BeerDetails/styles'
+
+type BeerDetailsPageProps = {
+  beer: Beer
+}
+
+const BeerDetailsPage = ({ beer }: BeerDetailsPageProps) => {
   return (
-    <Base>
-      <h1>Beer</h1>
-    </Base>
+    <>
+      <Head>
+        <title>{beer.name} | Beer Explorer</title>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Base>
+        <motion.div
+          initial={{ transform: 'translateY(100%)' }}
+          animate={{ transform: 'translateY(0px)' }}
+          exit={{ transform: 'translateY(100%)' }}
+        >
+          <S.BeerImage
+            src={beer.image_url ?? 'img/default_beer_image.svg'}
+            alt={beer.name}
+          />
+
+          <S.BeerTitle>{beer.name}</S.BeerTitle>
+
+          <S.Highlight>{beer.tagline}</S.Highlight>
+
+          <S.BeerDescriptionWrapper>
+            <S.BeerText>{beer.description}</S.BeerText>
+
+            <S.BeerText>
+              This beer was first brewed on {beer.first_brewed}
+            </S.BeerText>
+          </S.BeerDescriptionWrapper>
+
+          <S.SectionTitle>About this beer</S.SectionTitle>
+
+          <S.DetailsList>
+            <dt>
+              ABV <span>(Alcohol by volume)</span>
+            </dt>
+            <dd>{beer.abv}</dd>
+
+            <dt>
+              EBC <span>(European Brewery Convention)</span>
+            </dt>
+            <dd>{beer.ebc}</dd>
+
+            <dt>
+              SRM <span>(Standard Reference Method)</span>
+            </dt>
+            <dd>{beer.srm}</dd>
+
+            <dt>
+              IBU <span>(International Bittering Units)</span>
+            </dt>
+            <dd>{beer.ibu}</dd>
+
+            <dt>
+              OG <span>(Original Gravity)</span>
+            </dt>
+            <dd>{beer.target_og}</dd>
+
+            <dt>
+              FG <span>(Final Gravity)</span>
+            </dt>
+            <dd>{beer.target_fg}</dd>
+
+            <dt>PH</dt>
+            <dd>{beer.ph}</dd>
+          </S.DetailsList>
+
+          <S.ServedIngredients>
+            This beer is best served with the following ingredients :
+          </S.ServedIngredients>
+
+          <S.DiscList>
+            {beer.food_pairing.map((food, index) => (
+              <li key={index}>{food}</li>
+            ))}
+          </S.DiscList>
+
+          <S.SectionTitle>Your brewer&#39;s tips</S.SectionTitle>
+
+          <S.Highlight>
+            <blockquote>{beer.brewers_tips}</blockquote>
+          </S.Highlight>
+
+          <S.SectionTitle>Ingredients</S.SectionTitle>
+
+          <S.SubsectionTitle>Hops</S.SubsectionTitle>
+
+          <S.IngredientList>
+            {beer.ingredients.hops.map((hop, index) => (
+              <>
+                <dt key={index}>
+                  {hop.name} <span>({hop.attribute})</span>
+                </dt>
+                <dd>
+                  {hop.amount.value} {hop.amount.unit}
+                </dd>
+              </>
+            ))}
+          </S.IngredientList>
+
+          <S.IngredientList>
+            {beer.ingredients.malt.map((malt, index) => (
+              <>
+                <dt key={index}>{malt.name}</dt>
+                <dd>
+                  {malt.amount.value} {malt.amount.unit}
+                </dd>
+              </>
+            ))}
+          </S.IngredientList>
+
+          <S.SubsectionTitle>Malts</S.SubsectionTitle>
+
+          <S.IngredientList>
+            {beer.ingredients.malt.map((malt, index) => (
+              <>
+                <dt key={index}>{malt.name}</dt>
+                <dd>
+                  {malt.amount.value} {malt.amount.unit}
+                </dd>
+              </>
+            ))}
+          </S.IngredientList>
+
+          <S.SubsectionTitle>Yeast</S.SubsectionTitle>
+          <S.ServedIngredients>{beer.ingredients.yeast}</S.ServedIngredients>
+
+          <S.SectionTitle>Contributions</S.SectionTitle>
+          <S.Contributed>Contributed by {beer.contributed_by}.</S.Contributed>
+        </motion.div>
+      </Base>
+    </>
   )
 }
 
-export default Beer
+export const getServerSideProps: GetServerSideProps<BeerDetailsPageProps> = async (
+  context
+) => {
+  const response = await api.get<Beer[]>(`beers/${context.params!.beerId}`)
+
+  return { props: { beer: response.data[0] } }
+}
+
+export default BeerDetailsPage
